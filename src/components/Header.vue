ddf151f4d13b788e8b543ecfa36b7581a494eae4
ShunNakano <nakano.shun@lmi.ne.jp>
2023-11-02 17:04:48 +0900

    feat: Headerコンポーネント作成

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header_root">
+    <h1>HTML for a11y</h1>
+    <div class="image_wrapper">
+      <img src="/vite.svg" alt="vite logo" width="30" height="30" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header_root {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1200px;
+}
+.image_wrapper {
+  border-radius: 50%;
+  background-color: var(--white);
+}
+</style>
