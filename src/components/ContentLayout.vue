9ba3116ea51ed533e4568fe34589015717298b76
hakushun <hakushun.pianist@gmail.com>
2023-11-02 22:23:58 +0900

    feat: ContentLayoutコンポーネント作成

---
+++ src/components/ContentLayout.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <article :class="styles.root">
+    <h1 :class="styles.title">{{ title }}</h1>
+    <section :class="styles.body">
+      <slot />
+    </section>
+  </article>
+</template>
+
+<style module="styles">
+:root {
+  --gap: 32px;
+}
+.root {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  height: 100%;
+}
+.title {
+  font-size: 2rem;
+  text-decoration: underline;
+}
+.body {
+  position: relative;
+  display: grid;
+  grid-template-columns: calc(50% - var(--gap) / 2) calc(50% - var(--gap) / 2);
+  gap: var(--gap);
+  height: 100%;
+}
+.body::after {
+  position: absolute;
+  left: 50%;
+  content: '';
+  width: 1px;
+  height: 100%;
+  border: 1px solid var(--black);
+  transform: translateX(-50%);
+}
+</style>
