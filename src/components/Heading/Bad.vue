f83144ecc75cc3f2a54e178b97d35247fd1ee583
hakushun <hakushun.pianist@gmail.com>
2023-11-03 10:52:20 +0900

    feat: Heading/Bad.vueコンポーネント作成

---
+++ src/components/Heading/Bad.vue
@@ -0,0 +1,74 @@
+<template>
+  <article>
+    <span :class="styles.h1">リンクアンドモチベーション</span>
+    <div :class="styles.p">
+      株式会社リンクアンドモチベーション（英: Link and Motivation
+      Inc.）は、東京都中央区に本社を置く、経営コンサルティング会社。
+    </div>
+    <span :class="styles.h2">概要</span>
+    <div :class="styles.p">
+      2013年にはベンチャー企業の上場支援を開始した[1]。2016年には国内初の組織改善クラウドである「モチベーションクラウド」をリリース[2]。設立当初より、経営学・社会システム論・行動経済学・心理学を統合して考案された「モチベーションエンジニアリング[3]」を経営コンセプトとしてきた。
+    </div>
+    <span :class="styles.h3">基幹技術「モチベーションエンジニアリング」</span>
+    <div :class="styles.p">
+      設立当初より、経営学・社会システム論・行動経済学・心理学を統合して考案された、独自の基幹技術「モチベーションエンジニアリング[3]」を開発。
+    </div>
+    <span :class="styles.h3">事業内容</span>
+    <div :class="styles.p">
+      基幹技術「モチベーションエンジニアリング」を中心に、組織を対象にした「組織開発Division」、個人を対象にした「個人開発Division」、組織と個人をマッチングする「マッチングDivision」、「ベンチャーインキュベーション事業」で構成されている[4]。
+      <br />
+      企業を対象にした「組織開発Division」は、2000年創業時から提供している「コンサル・クラウド事業」に加え、そこから派生した「イベント・メディア事業」で構成されている。社員のモチベーションを成長エンジンとする企業のことを「モチベーションカンパニー[5]」と名付けている。
+      <br />
+      また、個人を対象にした「個人開発Division」は、2010年からスタートしており、「キャリアスクール事業」、「学習塾事業」で構成されている。これからの時代に求められる自立的・主体的に個人のキャリアを切り拓く人材を「アイカンパニー[6]」と名付けている。
+    </div>
+    <span :class="styles.h3">社名の由来</span>
+    <div :class="styles.p">
+      社名には、事業の中心テーマである「モチベーション」、そして個々人のモチベーションを組織の全体戦略に「リンク」させるという意味を込めている[7]。
+    </div>
+    <span :class="styles.h3">ロゴの意味</span>
+    <div :class="styles.p">
+      ロゴは、Link and
+      Motivationの「L」と「M」で構成されている。「L」と「M」を構成している一本一本の線は不揃いで単独では意味を成さない存在だが、マクロで見るとしっかりと「L」と「M」を表している。これは、一本一本の線は個人、「L」と「M」は全体（組織）を表しており、「One
+      for All, All for One」の思想を表現している[7]。
+    </div>
+    <span :class="styles.h2">サービス</span>
+    <span :class="styles.h3">クラウドサービス</span>
+    <div :class="styles.p">
+      2016年から国内初の組織改善クラウドサービス「モチベーションクラウド（MOTIVATION
+      CLOUD）」をリリースし、人工知能（AI）による組織データの有効活用を行っている。
+    </div>
+    <span :class="styles.h3">オウンドメディア</span>
+    <div :class="styles.p">
+      「コンサルティングファーム×研究所×コミュニティ」。3者のそれぞれの特徴を活かしながら、新しい意味の発信を行う「THE
+      MEANING OF WORK」を2020年リリース。モチベーションエンジニアリング研究所・Human Resouce
+      Committee (HRC)が運営する、組織や個人が「MEANING OF WORK
+      (働く意味)」を再定義するプロジェクト。[8]
+    </div>
+  </article>
+</template>
+
+<style module="styles">
+.h1 {
+  font-size: 2em;
+}
+.h2 {
+  font-size: 1.5em;
+}
+.h3 {
+  font-size: 1.17em;
+}
+.h1,
+.h2,
+.h3 {
+  display: block;
+  line-height: calc(1em + 0.5rem);
+  font-weight: bold;
+}
+.h2,
+.h3 {
+  margin-top: 8px;
+}
+.p {
+  margin-top: 4px;
+}
+</style>
