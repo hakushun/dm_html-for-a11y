813ecad4023ebe7b3e39258c7cdd4087cdc10af7
hakushun <hakushun.pianist@gmail.com>
2023-11-03 10:52:45 +0900

    feat: Heading page作成

---
+++ src/pages/Heading.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ContentLayout from '@/components/ContentLayout.vue';
+import Bad from '@/components/Heading/Bad.vue';
+import Good from '@/components/Heading/Good.vue';
+</script>
+
+<template>
+  <ContentLayout title="<h1> ~ <h6>">
+    <Bad />
+    <Good />
+  </ContentLayout>
+</template>
+
+<style module="styles"></style>
